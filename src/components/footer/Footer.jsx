b073f0ea96c8af00d96615c3213a207a33ee7cd4
SUSHANT <128069783+sushantttttt@users.noreply.github.com>
2024-01-06 00:35:34 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container flexSB'>
+          <div className='legal'>
+            <p>
+              2024 © Adminto theme by <span>sushant</span>
+            </p>
+          </div>
+          <div className='links flexSB'>
+            <li>About Us</li>
+            <li>Help</li>
+            <li>Contact Us</li>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/users/data.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+      id: 1,
+      cover: "https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?cs=srgb&dl=pexels-nitin-khajotia-1516680.jpg&fm=jpg",
+      title: "Sushant",
+      email: "sushant.jha@gmail.com",
+      user: "Admin",
+      time: "08:40 PM",
+    },
+    {
+      id: 2,
+      cover: "https://images.pexels.com/photos/1496647/pexels-photo-1496647.jpeg?cs=srgb&dl=pexels-hasib-khorami-1496647.jpg&fm=jpg",
+      title: "Anas",
+      email: "anasdakhan@gmail.com",
+      user: "Support Lead",
+      time: "01:34 PM",
+    },
+    {
+      id: 3,
+      cover: "https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Gaurav",
+      email: "gauravsharm@gmail.com",
+      user: "Designer",
+      time: "11:17 PM",
+    },
+    {
+      id: 4,
+      cover: "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-837140.jpg&fm=jpg",
+      title: "Deepanshu",
+      email: "pontutomarr@gmail.com",
+      user: "Developer",
+      time: "10:30 PM",
+    },
+  ]
+  
+  export default data
